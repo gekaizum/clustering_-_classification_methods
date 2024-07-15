a5f007bb35074b931ab263f995583284f74f52fb
Yevgeniy Gluhoy <128051209+gekaizum@users.noreply.github.com>
2024-07-15 23:28:17 +0300

    Add files via upload

--- unsupervized_learning0.m
+++ unsupervized_learning0.m
@@ -3,8 +3,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Parameters
 %m = 3; % length of vector (dimension)
-c = 4; % number of clusters
-colors = ['r', 'g', 'b','m','k']; % colors for clusters
+c = 6; % number of clusters
+colors = ['r', 'g', 'b','c','m','o','k']; % colors for clusters
 
 for m = 2:6
     for clusnum = 1:4
@@ -13,27 +13,28 @@ for m = 2:6
         labels = [];
         cluster_centers = [];
         for i = 1:c
-            n = randi([10,100]);% number of points in cluster
-            mean_val=randi([-20,20]);% mean value for cluster
-            std_devs=2*rand+0.1;% standart deviation for cluster
+            n = randi([10,100]); % number of points in cluster
+            mean_val = randi([-20,20], 1, m); % mean values for cluster
+            cov_matrix = wishrnd(eye(m), m); % random covariance matrix using Wishart distribution
+
             % Creating vector of cluster
-            cluster_vectors = mean_val + std_devs * randn(n, m);
+            cluster_vectors = mvnrnd(mean_val, cov_matrix, n);
+
             % Adding cluster to space matrix
             all_vectors = [all_vectors; cluster_vectors];
             % Adding labels for clusters
-            labels = [labels; repmat([i,n,mean_val,std_devs], n, 1)]; %cluster ID/number of points in cluster/mean value/standart deviation
+            labels = [labels; repmat([i, n, mean_val, cov_matrix(:)'], n, 1)]; % cluster ID / number of points in cluster / mean values / variances
+
             % Calculating center of cluster
             cluster_centers = [cluster_centers; mean(cluster_vectors, 1)];
         end
-        % Sort labels by the third column (center coordinates)
-        %all_vectors = sortrows(labels, 3);
         
         % Vizualization
-        if m==3
+        if m == 3
             figure;
             hold on;
             for i = 1:c
-                cluster_points = all_vectors(labels(:,1) == i, :);
+                cluster_points = all_vectors(labels(:, 1) == i, :);
                 scatter3(cluster_points(:, 1), cluster_points(:, 2), cluster_points(:, 3), 36, colors(i), 'filled');
                 % Grid line from center to points in cluster
                 for j = 1:size(cluster_points, 1)
@@ -48,16 +49,16 @@ for m = 2:6
             zlabel('Z');
             title('Clusters Visualization with Lines to Cluster Centers');
             grid on; 
-            view(3); % 3D vizualization
+            view(3); % 3D visualization
             xlim auto;
             ylim auto;
             zlim auto;
         end
-        if m==2
+        if m == 2
             figure;
             hold on;
             for i = 1:c
-                cluster_points = all_vectors(labels(:,1) == i, :);
+                cluster_points = all_vectors(labels(:, 1) == i, :);
                 scatter(cluster_points(:, 1), cluster_points(:, 2), 36, colors(i), 'filled');
                 % Grid line from center to points in cluster
                 for j = 1:size(cluster_points, 1)
@@ -75,7 +76,7 @@ for m = 2:6
             ylim auto;
         end
 
-        filename=['gaussian_cluster_', num2str(clusnum),'_',num2str(m),'D', '.mat'];
+        filename = ['gaussian_cluster_', num2str(clusnum), '_', num2str(m), 'D', '.mat'];
         save(filename);
     end
 end
