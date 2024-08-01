6fbf710614b2d809c43f1e07b428a92641fce00c
barerlich <94571281+barerlich@users.noreply.github.com>
2024-08-01 20:24:40 +0300

    Add files via upload

--- unsupervized_learning1_part2.m
+++ unsupervized_learning1_part2.m
@@ -1,11 +1,7 @@
-
 %% Gaussian PDF for vectors
 function res = pdf_of_cluster(cur_vector,mu_vector, sigma_val)
     cov_vector=zeros(length(cur_vector));
-    m=length(cur_vector);
-    for i = 0:m-1
-        cov_vector(i+1, :) = sigma_val(i*m+1:(i+1)*m);
-    end
+    cov_vector(1:length(cur_vector)+1:end) = sigma_val^2;
     if cond(cov_vector) < 1e3 % Check condition number
         inv_cov_vector = inv(cov_vector);
     else
@@ -88,56 +84,102 @@ for dimensionNum=2:6 %choose topology dimension
             xlim auto;
             ylim auto;
         end
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Q 1.2 only amount of clusters known %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Q 1.1 no mean value %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        current_cluster=[];
-        [unique_values, idx, ~] = unique(labels(:,1), 'stable');
-        merged_labels = labels(idx, :); %matrix of parameters: cluster num/size/mean/variance
+        % Initialization
+        mean_vector = repmat(mean(all_vectors, 1), c, 1); % Initial estimated mean values
+        deviation_vector = repmat(std(all_vectors, 1), c, 1); % Initial estimated standard deviation values
+        estim_aprior_prob = repmat(1 / c, c, 1); % Initial estimated apriori probabilities
         
-        aprior_prob=[merged_labels(:,1)];
-        aprior_prob=[aprior_prob,merged_labels(:,2)/sum(merged_labels(:,2))];
-        
-        mean_vector=repmat(mean(mean(all_vectors, 1)),c,m); %init vector of estimated mean values
+        maxIter = 100; % Number of iterations
         tolerance = 0.1;
+        
         % Tolerance matrix
-        random_offsets = tolerance * merged_labels(:,4) .* randn(c, 1);
+        random_offsets = tolerance * deviation_vector .* randn(c, m);
         mean_vector = mean_vector + random_offsets;
+        deviation_vector = deviation_vector + random_offsets .* mean_vector;
         
-        maxIter=100; % number of iterations of search for mean values
-        
-        %% MLE algorithm for finding mean value
         for iterNumber = 1:maxIter
-            new_mean_vector=zeros(c,m);
+            %% Aprioric probability estimation
+            new_aprior_prob = zeros(c, 1);
             for i = 1:c
-                mone=zeros(1,m);
-                mahane=zeros(1,m);
-                for j = 1: size(all_vectors,1)
-                    estimProb_i=pdf_of_cluster(all_vectors(j,:),mean_vector(i,:), merged_labels(i, 3+m:end));
-                    estimProb_i=estimProb_i*aprior_prob(i,2);
-                    total_estimProb_i=zeros(1,m);
+                end_prob = zeros(size(all_vectors, 1), 1);
+                for j = 1:size(all_vectors, 1)
+                    estimProb_i = pdf_of_cluster2(all_vectors(j, :), mean_vector(i, :), deviation_vector(i, :));
+                    estimProb_i = estimProb_i * estim_aprior_prob(i);
+                    total_estimProb_i = 0;
                     for try_mean = 1:c
-                        temp=pdf_of_cluster(all_vectors(j,:),mean_vector(try_mean,:), merged_labels(i, 3+m:end));
-                        temp=temp*aprior_prob(try_mean,1);
-                        total_estimProb_i=total_estimProb_i+temp;
+                        temp = pdf_of_cluster2(all_vectors(j, :), mean_vector(try_mean, :), deviation_vector(try_mean, :));
+                        temp = temp * estim_aprior_prob(try_mean);
+                        total_estimProb_i = total_estimProb_i + temp;
                     end
-                    end_prob=estimProb_i./total_estimProb_i;
-                    mone=mone+(end_prob.*all_vectors(j,:));
-                    mahane=mahane+end_prob;
+                    end_prob(j) = estimProb_i / total_estimProb_i;
                 end
-                new_mean_vector(i,:)=mone./mahane;
+                new_aprior_prob(i) = mean(end_prob);
             end
-            mean_vector=new_mean_vector;
+            total_prob = sum(new_aprior_prob);
+            if total_prob > 1
+                new_aprior_prob = new_aprior_prob / total_prob;
+            end
+        
+            %% Mean value estimation
+            new_mean_vector = zeros(c, m);
+            for i = 1:c
+                mone = zeros(1, m);
+                mahane = 0;
+                for j = 1:size(all_vectors, 1)
+                    estimProb_i = pdf_of_cluster2(all_vectors(j, :), mean_vector(i, :), deviation_vector(i, :));
+                    estimProb_i = estimProb_i * estim_aprior_prob(i);
+                    total_estimProb_i = 0;
+                    for try_mean = 1:c
+                        temp = pdf_of_cluster2(all_vectors(j, :), mean_vector(try_mean, :), deviation_vector(try_mean, :));
+                        temp = temp * estim_aprior_prob(try_mean);
+                        total_estimProb_i = total_estimProb_i + temp;
+                    end
+                    end_prob = estimProb_i / total_estimProb_i;
+                    mone = mone + (end_prob * all_vectors(j, :));
+                    mahane = mahane + end_prob;
+                end
+                new_mean_vector(i, :) = mone / mahane;
+            end
+        
+            %% Variance value estimation
+            new_deviation_vector = zeros(c, m);
+            for i = 1:c
+                mone = zeros(1, m);
+                mahane = 0;
+                for j = 1:size(all_vectors, 1)
+                    estimProb_i = pdf_of_cluster2(all_vectors(j, :), mean_vector(i, :), deviation_vector(i, :));
+                    estimProb_i = estimProb_i * estim_aprior_prob(i);
+                    total_estimProb_i = 0;
+                    for try_mean = 1:c
+                        temp = pdf_of_cluster2(all_vectors(j, :), mean_vector(try_mean, :), deviation_vector(try_mean, :));
+                        temp = temp * estim_aprior_prob(try_mean);
+                        total_estimProb_i = total_estimProb_i + temp;
+                    end
+                    end_prob = estimProb_i / total_estimProb_i;
+                    mone = mone + (end_prob * ((all_vectors(j, :) - mean_vector(i, :)) .^ 2));
+                    mahane = mahane + end_prob;
+                end
+                new_deviation_vector(i, :) = sqrt(mone / mahane);
+            end
+        
+            mean_vector = new_mean_vector;
+            deviation_vector = new_deviation_vector;
+            estim_aprior_prob = new_aprior_prob;
         end
+        
         %% Bayesian Decision Theory
         assigned_labels = zeros(size(all_vectors, 1), 1);
         for j = 1:size(all_vectors, 1)
             posterior_probs = zeros(c, 1);
             for i = 1:c
-                posterior_probs(i) = prod(pdf_of_cluster(all_vectors(j,:), mean_vector(i, :), merged_labels(i, m+3:end))) * aprior_prob(i, 2);
+                posterior_probs(i) = pdf_of_cluster2(all_vectors(j, :), mean_vector(i, :), deviation_vector(i, :)) * estim_aprior_prob(i);
             end
             [~, assigned_labels(j)] = max(posterior_probs);
-        end
+        end      
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
 %%%%%%%%%%%% Visualization of the result after Bayesian Decision Theory
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -223,7 +265,7 @@ for dimensionNum=2:6 %choose topology dimension
         end
         
         accuracy = correct_points / size(all_vectors, 1);
-        disp([filename,' accuracy for unknown mu: ', num2str(accuracy * 100), '%']);
+        disp([filename,' accuracy for unknown mu, sigma and P: ', num2str(accuracy * 100), '%']);
         %%
     end
 end
